33a7af698f34386c808bbff25b57a528d0e09657
markshim1986 <54639115+markshim1986@users.noreply.github.com>
2019-09-09 23:20:58 -0400

    Add files via upload

---
+++ Veterans Excel File for Workbook Assignment # 1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+Location = "datasets/Veterans Data for Workbook Assignment #1 (ISM 4402) (1).xls"
+df = pd.read_excel(Location)
+df
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
