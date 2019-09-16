e1365df282844968ce692610df563b6faae78270
markshim1986 <54639115+markshim1986@users.noreply.github.com>
2019-09-16 19:10:27 -0400

    Add files via upload

---
+++ Untitled9.py
@@ -0,0 +1,24 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+
+import pandas as pd
+import numpy as np
+import glob
+
+all_data = pd.DataFrame()
+for f in glob.glob("datasets/weekly_call_data/weekdata*.xlsx"):
+    print (f)
+    df = pd.read_excel(f)
+    all_data = all_data.append(df,ignore_index=True)  
+    all_data.describe()
+
+
+# In[3]:
+
+
+all_data.head()
+
